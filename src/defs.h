3a74639814804f66b5f4762293f5277e741c4b5d
sykim <mintlux@snu.ac.kr>
2024-05-31 18:01:23 +0900

    some parameters changed, and it seems to be working well so far

--- src/defs.h
+++ src/defs.h
@@ -46,7 +46,7 @@
 // KS regularlization related variables                                                                    
                                                                                                        
 #define stumpffN 12 // the order of approximation for stumpff functions                                    
-#define KSDistance 5e-3  // the distance of binary particles from each other                             
+#define KSDistance 3e-3  // the distance of binary particles from each other                             
 #define KSTime 2e-8  // short timestep criteria for binary search                                          
 #define PerturberDistance 0.01  // the distance of binary perturbers from the pair  
 
