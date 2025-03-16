46f39c7e4568bd489653f0962988b9954c233d57
cewgenius <cewgenius@snu.ac.kr>
2025-03-16 18:17:07 +0900

    minor change

--- src_mpi/ReadWrite.cpp
+++ src_mpi/ReadWrite.cpp
@@ -259,8 +259,8 @@ int writeParticle(double current_time, int outputNum) {
 		fprintf(stdout, "PID: %d. Position (pc) - x:%e, y:%e, z:%e, \n", members->PID, members->Position[0]*position_unit, members->Position[1]*position_unit, members->Position[2]*position_unit);
 		fprintf(stdout, "PID: %d. Velocity (km/s) - vx:%e, vy:%e, vz:%e, \n", members->PID, members->Velocity[0]*velocity_unit/yr*pc/1e5, members->Velocity[1]*velocity_unit/yr*pc/1e5, members->Velocity[2]*velocity_unit/yr*pc/1e5);
 		fprintf(stdout, "PID: %d. Mass (Msol) - %e, \n", members->PID, members->Mass*mass_unit);
-		fprintf(stdout, "NumNeighbor: %d, ACRadius: %e\n", members->NumberOfNeighbor, sqrt(members->RadiusOfNeighbor)*position_unit);
-        fprintf(stderr, "PID: %d. Total Acceleration - ax:%e, ay:%e, az:%e \n", members->PID, members->a_tot[0][0], members->a_tot[1][0], members->a_tot[2][0]);
+		fprintf(stdout, "PID: %d. NumNeighbor: %d, ACRadius: %e (pc)\n", members->PID, members->NumberOfNeighbor, sqrt(members->RadiusOfNeighbor)*position_unit);
+        fprintf(stdout, "PID: %d. Total Acceleration - ax:%e, ay:%e, az:%e \n", members->PID, members->a_tot[0][0], members->a_tot[1][0], members->a_tot[2][0]);
 		fprintf(stdout, "PID: %d. Reg Acceleration - ax:%e, ay:%e, az:%e, \n", members->PID, members->a_reg[0][0], members->a_reg[1][0], members->a_reg[2][0]);
 		fprintf(stdout, "PID: %d. Irr Acceleration - ax:%e, ay:%e, az:%e, \n", members->PID, members->a_irr[0][0], members->a_irr[1][0], members->a_irr[2][0]);
 		fprintf(stdout, "PID: %d. Time Steps (Myr) - irregular:%e, regular:%e \n", members->PID, members->TimeStepIrr*EnzoTimeStep*1e4, members->TimeStepReg*EnzoTimeStep*1e4);
@@ -270,8 +270,8 @@ int writeParticle(double current_time, int outputNum) {
 		fprintf(stdout, "PID: %d. Position (pc) - x:%e, y:%e, z:%e, \n", members->PID, members->Position[0]*position_unit, members->Position[1]*position_unit, members->Position[2]*position_unit);
 		fprintf(stdout, "PID: %d. Velocity (km/s) - vx:%e, vy:%e, vz:%e, \n", members->PID, members->Velocity[0]*velocity_unit/yr*pc/1e5, members->Velocity[1]*velocity_unit/yr*pc/1e5, members->Velocity[2]*velocity_unit/yr*pc/1e5);
 		fprintf(stdout, "PID: %d. Mass (Msol) - %e, \n", members->PID, members->Mass*mass_unit);
-		fprintf(stdout, "NumNeighbor: %d, ACRadius: %e\n", members->NumberOfNeighbor, sqrt(members->RadiusOfNeighbor)*position_unit);
-        fprintf(stderr, "PID: %d. Total Acceleration - ax:%e, ay:%e, az:%e \n", members->PID, members->a_tot[0][0], members->a_tot[1][0], members->a_tot[2][0]);
+		fprintf(stdout, "PID: %d. NumNeighbor: %d, ACRadius: %e (pc)\n", members->PID, members->NumberOfNeighbor, sqrt(members->RadiusOfNeighbor)*position_unit);
+        fprintf(stdout, "PID: %d. Total Acceleration - ax:%e, ay:%e, az:%e \n", members->PID, members->a_tot[0][0], members->a_tot[1][0], members->a_tot[2][0]);
 		fprintf(stdout, "PID: %d. Reg Acceleration - ax:%e, ay:%e, az:%e, \n", members->PID, members->a_reg[0][0], members->a_reg[1][0], members->a_reg[2][0]);
 		fprintf(stdout, "PID: %d. Irr Acceleration - ax:%e, ay:%e, az:%e, \n", members->PID, members->a_irr[0][0], members->a_irr[1][0], members->a_irr[2][0]);
 		fprintf(stdout, "PID: %d. Time Steps (Myr) - irregular:%e, regular:%e \n", members->PID, members->TimeStepIrr*EnzoTimeStep*1e4, members->TimeStepReg*EnzoTimeStep*1e4);
